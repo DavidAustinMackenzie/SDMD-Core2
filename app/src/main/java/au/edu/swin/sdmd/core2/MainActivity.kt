eaa323e1221e8dbf97b5c897a485b961620f1978
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2021-09-23 13:54:22 +1000

    Added images to imageViews

--- app/src/main/java/au/edu/swin/sdmd/core2/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/core2/MainActivity.kt
@@ -3,6 +3,10 @@ package au.edu.swin.sdmd.core2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+* Images sourced from: www.formula1.com
+* */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
