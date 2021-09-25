fcc5133eef581ad7db18e681824c2f855e6bab88
David Mackenzie <48002743+DavidAustinMackenzie@users.noreply.github.com>
2021-09-25 19:43:49 +1000

    Removed extra activites and make sure all inputs get values from objects

--- app/src/main/java/au/edu/swin/sdmd/core2/Location.kt
+++ app/src/main/java/au/edu/swin/sdmd/core2/Location.kt
@@ -1,4 +1,11 @@
 package au.edu.swin.sdmd.core2
 
-class Location {
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Location(
+    val country: String, val city: String,
+    val date:String, var rating: Float, val picture:Int
+): Parcelable {
 }
